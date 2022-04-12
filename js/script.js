f18d22a4cff360f1158558457b5cce7e750341f2
manzana00 <dev.manzana00@gmail.com>
2022-04-12 17:00:37 +0900

    반응형 마무리

--- js/script.js
+++ js/script.js
@@ -1,45 +1,54 @@
-window.onload = function(){
+window.onload = function () {
 
-       //  aos 관련
-        AOS.init({  
-        // once: true              
+    AOS.init({
+        once: true,
+        disable: function () {
+            var desktop = 1280;
+            return window.innerWidth < desktop;
+        }
     });
 
     // 모달창
     let modal_close = $('.modal-close');
     let modal = $('.modal');
 
-    modal_close.click(function(){
+    modal_close.click(function () {
         modal.hide();
     });
 
     let modal_bt = $('.modal-bt');
-    modal_bt.click(function(){
+    modal_bt.click(function () {
         modal.show();
     });
 
-    // 스크롤시 애니메이션    
+    // 스크롤시 애니메이션 
     $(".customer-box-cont").each(function (index, el) {
         new Waypoint({
             element: el,
             handler: function (direction) {
                 var element = $(this.element);
                 var delay = element.attr('data-delay');
-                setTimeout(function () {
-                    if (direction == "down") {
-                        element.addClass('slideUp');
-                        element.addClass('effect-op-active');
-                    } else {
-                        element.removeClass('slideUp');
-                        element.removeClass('effect-op-active');
-                    }
-                }, delay);
-                // this.destroy();
+
+                // 1280 이상일때
+                if ($(window).width() > 1280) {
+                    setTimeout(function () {
+                        if (direction == "down") {
+                            element.addClass('slideUp');
+                            element.addClass('effect-op-active');
+                        } else {
+                            element.removeClass('slideUp');
+                            element.removeClass('effect-op-active');
+                        }
+                    }, delay);
+                    // this.destroy();
+
+                } else {
+                    element.addClass('effect-op-active');
+                }
             },
             offset: '90%'
         });
     });
-
 
     $(".news-bottom").each(function (index, el) {
         new Waypoint({
@@ -47,21 +56,26 @@ window.onload = function(){
             handler: function (direction) {
                 var element = $(this.element);
                 var delay = element.attr('data-delay');
-                setTimeout(function () {
-                    if (direction == "down") {
-                        element.addClass('slideUp2');
-                        element.addClass('effect-op-active');
-                    } else {
-                        element.removeClass('slideUp2');
-                        element.removeClass('effect-op-active');
-                    }
-                }, delay);
-                // this.destroy();
+                // 1280 이상일때
+                if ($(window).width() > 1280) {
+                    setTimeout(function () {
+
+                        if (direction == "down") {
+                            element.addClass('slideUp2');
+                            element.addClass('effect-op-active');
+                        } else {
+                            element.removeClass('slideUp2');
+                            element.removeClass('effect-op-active');
+                        }
+                    }, delay);
+                    //   this.destroy();
+                } else {
+                    element.addClass('effect-op-active');
+                }
             },
             offset: '90%'
         });
     });
-
 
     $(".partner-link").each(function (index, el) {
         new Waypoint({
@@ -69,17 +83,21 @@ window.onload = function(){
             handler: function (direction) {
                 var element = $(this.element);
                 var delay = element.attr('data-delay');
-                setTimeout(function () {
-
-                    if (direction == "down") {
-                        element.addClass('slideUp');
-                        element.addClass('effect-op-active');
-                    } else {
-                        element.removeClass('slideUp');
-                        element.removeClass('effect-op-active');
-                    }
-                }, delay);
-              //   this.destroy();
+                // 1280 이상일때
+                if ($(window).width() > 1280) {
+                    setTimeout(function () {
+                        if (direction == "down") {
+                            element.addClass('slideUp');
+                            element.addClass('effect-op-active');
+                        } else {
+                            element.removeClass('slideUp');
+                            element.removeClass('effect-op-active');
+                        }
+                    }, delay);
+                    //   this.destroy();
+                } else {
+                    element.addClass('effect-op-active');
+                }
             },
             offset: '90%'
         });
@@ -90,17 +108,29 @@ window.onload = function(){
     let gnb_li = $('.gnb > li');
     let submenu_div = $('.submenu-div')
     let submenu_box = $('.submenu-box');
+
     // 서브메뉴 높이 값
     let submenu_height = [];
+
     // 서브메뉴 높이 값 파악
-    $.each(submenu_box, function(index, item){
+    $.each(submenu_box, function (index, item) {
         let temp = $(this).outerHeight();
         temp = Math.ceil(temp);
-        submenu_height.push(temp);
         submenu_height[index] = temp;
-    }); 
-    $.each(gnb_li, function(index, item){
-        $(this).mouseenter(function(){
+    });
+
+    // 리사이징 할때마다 계산
+    $(window).resize(function(){ 
+        console.log('게산')       
+        $.each(submenu_box, function (index, item) {
+            let temp = $(this).outerHeight();
+            temp = Math.ceil(temp);
+            submenu_height[index] = temp;
+        });
+    });
+
+    $.each(gnb_li, function (index, item) {
+        $(this).mouseenter(function () {
             // 높이값을 파악한다.
             let h = submenu_height[index];
             // 높이값을 적용한다.
@@ -114,20 +144,21 @@ window.onload = function(){
             gnb_li.eq(index).addClass('gnb-li-active');
         });
     });
+
     // 서브메뉴 숨기기
     let nav = $('.nav');
     // 서브메뉴 타이머
     let menu_timer;
     let menu_timer_delay = 100;
-    nav.mouseenter(function(){
+    nav.mouseenter(function () {
         clearTimeout(menu_timer);
     });
-    nav.mouseleave(function(){
+    nav.mouseleave(function () {
         clearTimeout(menu_timer);
         menu_timer = setTimeout(menuUp, menu_timer_delay);
     });
 
-    function menuUp(){
+    function menuUp() {
         clearTimeout(menu_timer);
         submenu_div.css('height', 0);
         submenu_div.css('border-bottom-width', 0);
@@ -137,26 +168,12 @@ window.onload = function(){
 
     // 서브메뉴 묶음
     let submenu_container = $('.submenu-div .container');
-    submenu_container.mouseenter(function(){
+    submenu_container.mouseenter(function () {
         clearTimeout(menu_timer);
     });
-    submenu_container.mouseleave(function(){
+    submenu_container.mouseleave(function () {
         clearTimeout(menu_timer);
         menu_timer = setTimeout(menuUp, menu_timer_delay);
-    });
-
-    // 공지사함 탭
-    let news_menu = $('.news-menu a');
-    let news_list = $('.news-list');
-    $.each(news_menu, function(index, item){
-        $(this).click(function(event){
-            // a href 막기
-            event.preventDefault();
-            news_menu.removeClass('news-menu-active')
-            news_menu.eq(index).addClass('news-menu-active');
-            news_list.removeClass('news-list-active');
-            news_list.eq(index).addClass('news-list-active');
-        });
     });
 
     let sw_vs_notice = new Swiper('.sw-vs-notice', {
@@ -196,32 +213,33 @@ window.onload = function(){
         speed: 500,
         pagination: {
             el: '.sw-news-pg',
-            clickable : true,
+            clickable: true,
         },
     });
-    $('.sw-news-bt').click(function(){
+    $('.sw-news-bt').click(function () {
 
         // 현재 sw-news-bt-active 가 없으면
         // 자동 실행 중이다.
 
         // sw-news-bt-active 가 있으면
         // 멈춘 상태이다.
-        
+
         let state = $(this).hasClass('sw-news-bt-active');
 
-        if(state == true) {
+        if (state == true) {
             // 현재 멈추고 있으니 플레이 상태로 바꾼다.
             sw_news.autoplay.start();
-        }else{
+        } else {
             // 현재 플레이 중이니 멈춤 상태로 바꾼다.
             sw_news.autoplay.stop();
         }
         $(this).toggleClass('sw-news-bt-active');
     });
 
-    let sw_banner = new Swiper('.sw-banner', { 
+    // 베너 슬라이드
+    let sw_banner = new Swiper('.sw-banner', {
         loop: true,
-        slidesPerView: 7,                             
+        slidesPerView: 3,
         autoplay: {
             delay: 1000,
             disableOnInteraction: false,
@@ -230,17 +248,42 @@ window.onload = function(){
         navigation: {
             nextEl: '.banner-prev',
             prevEl: '.banner-next'
+        },
+        breakpoints: {
+            760: {
+                slidesPerView: 4,
+            },
+            900: {
+                slidesPerView: 5,
+            },
+            1200: {
+                slidesPerView: 7,
+            }
         }
     });
 };
 
-$(document).ready(function(){
+$(document).ready(function () {
+
+    // 공지사항 탭
+    let news_menu = $('.news-menu a');
+    let news_list = $('.news-list');
+    $.each(news_menu, function (index, item) {
+        $(this).click(function (event) {
+            // a href 막기
+            event.preventDefault();
+            news_menu.removeClass('news-menu-active')
+            news_menu.eq(index).addClass('news-menu-active');
+            news_list.removeClass('news-list-active');
+            news_list.eq(index).addClass('news-list-active');
+        });
+    });
 
     // 장면을 저장합니다.
-    let section =$('.main > section');
+    let section = $('.main > section');
     // 각각의 위치를 저장한다.
     let section_pos = [];
-    $.each(section, function(index, item){
+    $.each(section, function (index, item) {
         // 위치값을 파악한다.
         let temp = $(this).offset().top;
         // 정수로 만든다.
@@ -256,12 +299,46 @@ $(document).ready(function(){
 
     // 화면이 리사이징 될 때
     // section_pos 배열을 업데이트 한다.
-    $(window).resize(function(){
+
+    // 마우스 휠을 화면 이동에 사용할지 말지를 결정해 주는 변수
+    // 1 이면 휠이 적용이 되고 0이면 휠이 적용이 안된다.
+    let wheel_active = 1;
+    let window_width = $(window).width();
+    // 만약에 윈도우 화면 너비가 1280px과 같거나 크면
+    // 휠 모션을 활성화 하겠다.
+    if (window_width >= 1280) {
+        wheel_active = 1;
+    } else {
+        wheel_active = 0;
+    }
+
+    $(window).resize(function () {
+        window_width = $(window).width();
+        if (window_width >= 1280) {
+            wheel_active = 1;
+            // 보도자료 자동 포커스
+
+            // 1280 에서
+            // 보여서는 안되는 공지사항이
+            // news-menu-active 를 가지고 있는지 판단
+            let temp = news_menu.eq(0).hasClass('news-menu-active');
+
+            if (temp == true) {
+                news_menu.eq(1).trigger('click');
+            }
+
+        } else {
+            wheel_active = 0;
+        }
+        // 1280px 이상이면 휠 모션을 하지 않는다. (반응형)
+        // 이동하는 모션의 스크롤 위치를 계산할 필요가 없다.
+        return;
+
         // 장면을 저장합니다.
-        let section =$('.main > section');
+        let section = $('.main > section');
         // 각각의 위치를 저장한다.
         let section_pos = [];
-        $.each(section, function(index, item){
+        $.each(section, function (index, item) {
             // 위치값을 파악한다.
             let temp = $(this).offset().top;
             // 정수로 만든다.
@@ -273,7 +350,7 @@ $(document).ready(function(){
         // footer 빠진 상태로 저장
         let footer_pos = $('.footer').offset().top;
         footer_pos = parseInt(footer_pos);
-        section_pos[section_total-1] = (footer_pos);
+        section_pos[section_total - 1] = (footer_pos);
 
         // section_index가 변할 필요가 없다.
         // 휠을 돌린 것은 아니니까
@@ -297,32 +374,38 @@ $(document).ready(function(){
     // console.log(section_total);
 
     // 윈도우 스크롤 처리
-    $(window).scroll(function(){
+    $(window).scroll(function () {
 
     });
 
     // 마우스 휠 처리
-    $(window).bind('mousewheel DOMMouseScroll', function (event){
+    $(window).bind('mousewheel DOMMouseScroll', function (event) {
         let distance = event.originalEvent.wheelDelta;
         if (distance == null) {
             distance = event.originalEvent.detail * -1;
         }
-        
+
+        // 화면 너비에 따라서 휠 모션 적용 여부 결정
+        if (wheel_active == 0) {
+            // 화면이 1280px 보다 작으면 휠 모션을 하지 않겠다.
+            return;
+        }
+
         // 연속으로 휠이 들어온 경우 처리
-        if(section_scroll == 1){
+        if (section_scroll == 1) {
             return;
         }
 
         section_scroll = 1;
 
-        if(distance < 0) {
-            section_index ++;
-            if(section_index >= section_total) {
+        if (distance < 0) {
+            section_index++;
+            if (section_index >= section_total) {
                 section_index = section_total - 1;
             }
-        }else{
-            section_index --;
-            if(section_index < 0){
+        } else {
+            section_index--;
+            if (section_index < 0) {
                 section_index = 0;
             }
         }
@@ -332,15 +415,15 @@ $(document).ready(function(){
 
     // 포커스 유지
     let control_menu = $('.control-menu a');
-    $.each(control_menu, function(index, item) {
-            $(this).click(function(event) { 
-                event.preventDefault();
-                section_index = index;
-                sectionFn();
-            });
+    $.each(control_menu, function (index, item) {
+        $(this).click(function (event) {
+            event.preventDefault();
+            section_index = index;
+            sectionFn();
         });
+    });
 
-    function sectionFn(){
+    function sectionFn() {
 
         let temp = section_pos[section_index];
 
@@ -350,7 +433,7 @@ $(document).ready(function(){
 
         $('html').animate({
             scrollTop: temp,
-        }, section_speed, function(){
+        }, section_speed, function () {
             // 모션이 완료된 시점
             section_scroll = 0;
             // 클릭이 가능하도록 처리
@@ -362,47 +445,47 @@ $(document).ready(function(){
     // 최초 한번 실행
     sectionFn();
 
-    
+
     // 위로가기 아래로가기
-    let gotop = $('.gotop');    
-    let goup = $('.goup');   
+    let gotop = $('.gotop');
+    let goup = $('.goup');
     let godown = $('.godown');
     // 연속 버튼 막기
     let bt_bool = false;
-    
-    gotop.click(function(event){
+
+    gotop.click(function (event) {
         event.preventDefault();
         section_index = 0;
         sectionFn();
     });
 
-    goup.click(function(event){
+    goup.click(function (event) {
         event.preventDefault();
 
         // 움직이고 있는 동안에는 클릭을 막는다.
-        if(bt_bool == true){
+        if (bt_bool == true) {
             return;
         }
         bt_bool == true;
 
         section_index--;
-        if(section_index < 0) {
+        if (section_index < 0) {
             section_index = 0;
         }
         sectionFn();
     });
 
-    godown.click(function(event){
+    godown.click(function (event) {
         event.preventDefault();
 
         // 움직이고 있는 동안에는 클릭을 막는다.
-        if(bt_bool == true){
+        if (bt_bool == true) {
             return;
         }
         bt_bool == true;
 
         section_index++;
-        if(section_index >= section_total - 1) {
+        if (section_index >= section_total - 1) {
             section_index = section_total - 2
         }
         sectionFn();
